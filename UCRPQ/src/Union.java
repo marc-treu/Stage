790bcb5ac0064e15a1a834cb927782e3c24164e6
Treu Marc <treu@informatique.univ-paris-diderot.fr>
2018-06-06 11:58:34 +0200

    Union.java, ajout du constructeur

--- UCRPQ/src/Union.java
+++ UCRPQ/src/Union.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
 
 public class Union extends NAry {
-
+	
+	
+	public Union(RegExp... args) {
+		
+		this.children = new ArrayList<RegExp>();
+		
+		for(int i=0;i<args.length;++i){
+			this.children.add(args[i]);
+		}	
+	}
+	
 	@Override
 	public Type type() {
 		return Type.Union;
