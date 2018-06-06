aac9fc211a47d91731624b9485d1dd91380c8410
Treu Marc <treu@informatique.univ-paris-diderot.fr>
2018-06-06 13:42:30 +0200

    suppression des fichier de recommenders

---
+++ UCRPQ/src/RPQ.java
@@ -0,0 +1,19 @@
+
+public class RPQ {
+	
+	String origin;
+	String destination;
+	RegExp expression;
+	
+	
+	public RPQ(String origin, RegExp expression, String destination){
+		this.origin = origin;
+		this.destination = destination;
+		this.expression = expression;
+	}
+	
+	public String toCypher(){
+		return null;
+	}
+	
+}
