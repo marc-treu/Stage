62e2e3bfba671e952a00529aec3b71388d887cf7
Treu Marc <treu@informatique.univ-paris-diderot.fr>
2018-06-06 11:59:47 +0200

    Concatenation.java, ajout du constructeur

--- UCRPQ/src/Concatenation.java
+++ UCRPQ/src/Concatenation.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
 
 public class Concatenation extends NAry {
 
+	
+	public Concatenation(RegExp... args) {
+		
+		this.children = new ArrayList<RegExp>();
+		
+		for(int i=0;i<args.length;++i){
+			this.children.add(args[i]);
+		}	
+	}	
+	
 	@Override
 	public Type type() {
 		return Type.Concatenation;
