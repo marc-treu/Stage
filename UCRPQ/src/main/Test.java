59383b766c4a1c2013a9eeb52cc5c5ebde50ad74
Treu Marc <treu@informatique.univ-paris-diderot.fr>
2018-06-21 16:12:06 +0200

    Test, Suppression des premiers teste pour ne garder que ceux qui utilise le parser, et ajout des testes pour les UCRPQ

--- UCRPQ/src/main/Test.java
+++ UCRPQ/src/main/Test.java
@@ -11,71 +11,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		
-		// x,a,y
-		RPQ rpq1 = new RPQ("x",
-					new Atom("a"),
-					"y");
-		System.out.println(rpq1);
-		System.out.println(rpq1.toCypher());
-
-
-		// x,a.b,y
-		RPQ rpq2 = new RPQ("x",
-				new Concatenation(new Atom("a"),new Atom("b")),
-				"y");
-		System.out.println(rpq2);
-		System.out.println(rpq2.toCypher());
-
-
-		// x,(a.b-),y
-		RPQ rpq3 = new RPQ("x",
-					new Concatenation(new Atom("a"), new Atom("b",false)),
-					"y");
-		System.out.println(rpq3);
-		System.out.println(rpq3.toCypher());
-
-
-		// x,((a+b-).c),y
-		RPQ rpq4 = new RPQ("x",
-				new Concatenation(
-				new Union(new Atom("a"),new Atom("b",false)),new Atom("c")),
-				"y");
-		System.out.println(rpq4);
-		System.out.println(rpq4.toCypher());
-
-		RPQ rpq5 = new RPQ("x",
-				new Concatenation(
-				new Union(new Atom("a"),new Atom("b")),new Atom("c",false)),
-				"y");
-		System.out.println(rpq5);
-		System.out.println(rpq5.toCypher());
-
-		RPQ rpq6 = new RPQ("x",
-				new Concatenation(
-				new Union(new Atom("a"),new Atom("a",false)),new Atom("c",false)),
-				"y");
-		System.out.println(rpq6);
-		System.out.println(rpq6.toCypher());
-
-		RPQ rpq7 = new RPQ("x",
-				new Union(new Atom("a"),new Union(new Atom("b"),new Atom("c"))),
-				"y");
-		System.out.println(rpq7);
-		System.out.println(rpq7.toCypher());
-
-		RPQ rpq8 = new RPQ("x",
-				new Star(new Atom("x")),
-				"y");
-		System.out.println(rpq8);
-		System.out.println(rpq8.toCypher());
-
-		RPQ rpq9 = new RPQ("x",
-				new Star(new Union(new Atom("a"),new Atom("b",false),new Atom("b"))),
-				"y");
-		//System.out.println(rpq9);
-		//System.out.println(rpq9.toCypher());
-
+		System.out.println("####  RPQ  ####\n");
 
 
 	    List<String> rpq_strings
@@ -98,6 +34,7 @@ public class Test {
 			System.out.println(rpq.toCypher());
 		}
 
+		System.out.println("\n####  CRPQ  ####\n");
 		
 		List<String> crpq_string 
 			= Arrays.asList("(x,(a.b.c),y)&(x,a*,y)",
@@ -109,7 +46,21 @@ public class Test {
 			System.out.println(cr.toCypher());
 		}
 		
-		//System.out.println(Parser.parseUCRPQ("(x,(a+b),y)&x,(c-),y|x,(a+b),y"));
+
+		System.out.println("\n####  UCRPQ  ####\n");
+
+		
+		List<String> ucrpq_string 
+			= Arrays.asList("(x,(a.b.c),y)&(x,a*,y)|(x,(a*.(c+(b.d))),y)",
+					"(x,(a+b),y)&x,(c-),y|x,(a+b),y");
+		
+		List<UCRPQ> ucrpqs = ucrpq_string.stream().map(Parser::parseUCRPQ).collect(Collectors.toList());
+		for (UCRPQ ucr : ucrpqs) {
+			System.out.println("\n"+ucr.toString());
+			System.out.println(ucr.toCypher());
+		}
+		
+		
 		
 	}
 }
