9339a791a20c9c9543da0253803677e6872bf831
Treu Marc <treu@informatique.univ-paris-diderot.fr>
2018-06-22 15:56:12 +0200

    Union, Correction de conflit pour revenir a la version sans l'implemetation de l'étoile borné

--- UCRPQ/src/main/Union.java
+++ UCRPQ/src/main/Union.java
@@ -12,11 +12,11 @@ public class Union extends NAry {
 
 	int direction=-1; // 0 si inverser, 1 si normale, 2 si non orienté et -1 si non défini
 
-	
+
 	public Union(RegExp... args) {
 		super("+",args);
 	}
-	
+
 	@Override
 	public Type type() {
 		return Type.Union;
@@ -29,19 +29,20 @@ public class Union extends NAry {
 
 	@Override
 	public String toCypher() {
-		return this.toCypher(false,0);
+		return this.toCypher(false);
 	}
-	
-	public String toCypher(boolean fromStar, int borneSup) {
+
+	public String toCypher(boolean fromStar) {
 		// Initialisation
 		StringBuilder sb = new StringBuilder();
 		Set<String> hs = new HashSet<String>();
+ 
 
-
-		if(this.direction==-1)// Si la direction n'est pas encore defini, normalement isCypherable() est appeler avant toCypher()
+		if(this.direction==-1)// Si la direction n'est pas encore defini, normalement, isCypherable() est appeler avant toCypher()
 			this.isCypherable();// La direction des atoms est donnée dans isCypherable()
-		
+
 		sb.append( this.direction==0 ? "<-[:" : "-[:");
+
 		sb.append(((Atom)this.children.get(0)).getEtiquette());
 		hs.add(((Atom)this.children.get(0)).getEtiquette());
 
@@ -52,53 +53,51 @@ public class Union extends NAry {
 				sb.append(((Atom)this.children.get(i)).getEtiquette());
 			}
 		}
-		
-		if(!fromStar)
-			sb.append( this.direction==1 ? "]->" : "]-" );
-		else 
-			sb.append( borneSup > 0 ? (this.direction==1 ? "*.."+borneSup+"]->" : "*.."+borneSup+"]-") : (this.direction==1 ? "*]->" : "*]-"));
+
+
+		sb.append( fromStar ? (this.direction==1 ? "*]->" : "*]-") : (this.direction==1 ? "]->" : "]-"));
 		return sb.toString();
 	}
 
 	@Override
 	public boolean isCypherable() {
-		
+
 		//Iniatialisation
 		Map<String, Integer> hm = new HashMap<>();
 		String tmp_etiquette = "\0";
-		
+
 		for (RegExp e : this.children) {
-			if(e.type()!=RegExp.Type.Atom) 
+			if(e.type()!=RegExp.Type.Atom)
 				return false;
-			
+
 			tmp_etiquette =((Atom)e).getEtiquette();
-			
+
 			if(hm.containsKey(tmp_etiquette)) { // si l'etiquette est deja dans le set
-				if (hm.get(tmp_etiquette).equals(((Atom)e).getDirection() ? 1 : 0 )== false 
+				if (hm.get(tmp_etiquette).equals(((Atom)e).getDirection() ? 1 : 0 )== false
 							&& hm.get(tmp_etiquette)!=2)
 					hm.put(tmp_etiquette, 2);
 			}
-			else 
-				hm.put(tmp_etiquette, ((Atom)e).getDirection() ? 1 : 0);			
-		}			
+			else
+				hm.put(tmp_etiquette, ((Atom)e).getDirection() ? 1 : 0);
+		}
 
 		// On teste si tous les Atom sont dans la même direction
-		for (Map.Entry<String, Integer> entry : hm.entrySet()) 
+		for (Map.Entry<String, Integer> entry : hm.entrySet())
 			if (entry.getValue() != hm.get(tmp_etiquette))
 				return false;
-		
-		// On initialisation la direction des etiquettes 
+
+		// On initialisation la direction des etiquettes
 		this.direction = hm.get(tmp_etiquette);
-		
+
 		return true;
 	}
-	
+
 	public int getDirection() {
 		if(this.direction==-1)// Si la direction n'est pas encore defini, normalement, isCypherable() est appeler avant toCypher()
 			this.isCypherable();// La direction des atoms est donnée dans isCypherable()
 		return this.direction;
 	}
-	
+
 	@Override
 	public Union flatten() {
 		Set<String> hs = new HashSet<String>();
@@ -111,5 +110,5 @@ public class Union extends NAry {
 		}
 		return new Union(resultat.toArray(new RegExp [resultat.size()]));
 	}
-	
+
 }
