efac8319912cac4ee049f90d70ba67a6b898460f
Treu Marc <treu@informatique.univ-paris-diderot.fr>
2018-07-24 12:44:06 +0200

    MonoideTransition, modification des la List<String>, pour un String []

--- UCRPQ/src/main/MonoideTransition.java
+++ UCRPQ/src/main/MonoideTransition.java
@@ -1,42 +1,43 @@
 package main;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 public class MonoideTransition {
 
 
-	HashMap<String,List<String>> mot;
-	HashMap<List<String>,String> listemot;
-	HashMap<String,String> listeregle;
+	HashMap<String,String[]> mot;
+	HashMap<String[],String> listeMot;
+	HashMap<String,String> listeRegle;
 	
 	public MonoideTransition() {
 		this.mot = new HashMap<>();
-		this.listemot = new HashMap<>();
-		this.listeregle = new HashMap<>();
+		this.listeMot = new HashMap<>();
+		this.listeRegle = new HashMap<>();
 	}
 	
-	public void addmot(String mot, List<String> correspondant) {
+	public void addMot(String mot, String[] correspondant) {
 		this.mot.put(mot,correspondant);
-		this.listemot.put(correspondant,mot);
+		this.listeMot.put(correspondant,mot);
 	}
 	
-	public void addliste(List<String> correspondant,String mot) {
-		this.listemot.put(correspondant,mot);
+	public void addListe(String[] correspondant,String mot) {
+		this.listeMot.put(correspondant,mot);
 		this.mot.put(mot,correspondant);
 	}
 
-	public boolean containtListeMot(List<String> correspond) {
-		return listemot.containsKey(correspond);
+	public boolean containtListeMot(String[] correspond) {
+		System.out.println(this+"\n"+Arrays.toString(correspond));
+		return listeMot.containsKey(correspond);
 	}
 
-	public void addregle(String motlong, List<String> correspond) {
-		listeregle.put(motlong, listemot.get(correspond));			
+	public void addRegle(String motlong, String[] correspond) {
+		listeRegle.put(motlong, listeMot.get(correspond));			
 	}
 	
 	@Override
 	public String toString() {
-		return mot.toString()+"\n"+listeregle.toString();
+		return mot.toString()+"\n"+listeRegle.toString();
 	}		
 
 }
